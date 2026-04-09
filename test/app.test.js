3a2f8b5c8ba5a4ed3122f40af1258fa4b167eb35
Leidy <solarteleidy690@gmail.com>
2026-04-09 17:12:53 -0500

    subiendo cambios

--- test/app.test.js
+++ test/app.test.js
@@ -1,30 +1,56 @@
-import {saludar, despedir, estadoSistema} from "../src/app.js"
+import {
+  saludar,
+  despedir,
+  estadoSistema,
+  suma,
+  restar
+} from "../src/app.js";
 
-function ejecutarPruebas(){
-let pasadas =0;
-let fallidas =0;
+function ejecutarPruebas() {
+  let pasadas = 0;
+  let fallidas = 0;
 
-const r1 = saludar("Rider")
-if(r1.includes("Rider")){
-console.log("Test 1 pasado: funcion saludar en codeRider es corecta")
-pasadas++;
-} else {
-console.log("Test 1 Fallido: ", r1);
-fallidas++;
+  const r1 = saludar("Leidy");
+  if (r1.includes("Leidy")) {
+    console.log("Test 1 pasado: saludar() funciona correctamente");
+    pasadas++;
+  } else {
+    console.log("Test 1 fallido:", r1);
+    fallidas++;
+  }
 
-	}
-const actualEstado = estadoSistema();
-if(actualEstadado.estado === "activo" ){
-console.log("Test 2: Pasado: estadoSistema() funciona en codeRider");
-pasadas++;
+  const actualEstado = estadoSistema();
+  if (actualEstado.estado === "activo") {
+    console.log("Test 2 pasado: estadoSistema() funciona correctamente");
+    pasadas++;
+  } else {
+    console.log("Test 2 fallido:", actualEstado);
+    fallidas++;
+  }
 
-} else {
-console.log("Test 2 fallido: ", estado, "en codeRider");
-fallidas++;
+  const r3 = suma(5, 3);
+  if (r3 === 8) {
+    console.log("Test 3 pasado: suma() funciona correctamente");
+    pasadas++;
+  } else {
+    console.log("Test 3 fallido:", r3);
+    fallidas++;
+  }
+
+  const r4 = restar(10, 4);
+  if (r4 === 6) {
+    console.log("Test 4 pasado: restar() funciona correctamente");
+    pasadas++;
+  } else {
+    console.log("Test 4 fallido:", r4);
+    fallidas++;
+  }
+
+  console.log(`\nResultados: ${pasadas} pasadas, ${fallidas} fallidas`);
+
+  if (fallidas > 0) {
+    process.exit(1);
+  }
 }
-console.log ("\nResultados: " + pasadas + " pasadas, ", + fallidas + "fallidas");
-if(fallidas>0) precess.exit(1); "fallidas"
-}
+
 ejecutarPruebas();
-
-}
